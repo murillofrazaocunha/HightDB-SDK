3c5a0ba06dd6f6bf174f7ed01d349766c31da604
Murillo <pessoalmurillofrazao@gmail.com>
2025-04-18 21:08:06 -0300

    initial commit

--- index.ts
+++ index.ts
@@ -151,6 +151,7 @@ export class HightDB implements HightDBClient {
             });
 
             this.client.once('error', (err) => {
+                console.log("amigao")
                 reject(err);
             });
         });
